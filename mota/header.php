c60338ebeceb78f2efd822febc06b00dd5586646
ChaLeVerdier <contact@art-graphic-leverdier.com>
2024-10-11 16:46:01 +0200

    Importation du thème TwentyTwentyOne. Récupération de page.php et single.php pour définir la structure de toutes nos pages et articles. Utilisation de Sass comme dans le projet Koukaki, avec Node.js pour automatiser la compilation. Génération du fichier package.json pour gérer les tâches. Création d'un dossier scss pour stocker les styles, qui seront automatiquement compilés en assets/css/main.css.

--- mota/header.php
+++ mota/header.php
@@ -8,9 +8,23 @@
 
 
 <body>
+<header>
 
+<nav>
+<img src="assets/images/logo.png" alt="Logo du site">
+<ul> 
+     <li><a href="#">Accueil</a></li>
+     <li><a href="#">A propos</a></li>
+     <li><a href="#">Contact</a></li>
+</ul>
 
+</nav>
 <h1>
      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/titre_header.png" alt="Titre du site">
+     
 </h1>
+
+</header>
+
+
      
